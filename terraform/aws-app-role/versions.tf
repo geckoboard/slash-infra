b3529eb944886a7ad2199ca43ba3628a660864e5
Kyrill <1942093+poolski@users.noreply.github.com>
2021-12-17 13:09:39 +0000

    Update AWS provider constraints

--- terraform/aws-app-role/versions.tf
+++ terraform/aws-app-role/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.16"
+      version = "~> 3.10"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.9"
 }
